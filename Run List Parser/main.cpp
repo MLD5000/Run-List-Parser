be55bfe8e5d2e2013f4c0ba06f53b0c5a6577958
MLD5000 <mattldanner@gmail.com>
2014-09-11 19:02:42 -0500

    Added project file

---
+++ Run List Parser/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string runList;
+
+    cout << "Enter Run List (Hex): ";
+    getline(cin, runList);
+
+    for ( int i = 0; i < runList.length(); i++ ){
+
+
+
+
+    }
+
+
+
+
+
+
+
+    return 0;
+}
