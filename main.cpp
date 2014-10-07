e73b41bda16ce902ff7ad7a6ba8cdc711c34c14b
MLD5000 <mattldanner@gmail.com>
2014-10-07 18:43:18 -0500

    Updated code, fixed major loop bug, and added code comments

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
     Programmer:     Matt Danner
     Program Name:   Run List Parser
-    version:        1.0
+    version:        1.2
 
     Description:
     A user can input the hexadecimal values of a run list from
@@ -31,175 +31,213 @@ int main(){
 
     cout << "Program Name:   Run List Parser" << endl;
     cout << "Programmer:     Matt Danner" << endl;
-    cout << "Version:        1.0" << endl << endl;
+    cout << "Version:        1.2" << endl << endl;
 
     cout << "Instructions: " << endl;
-    cout << "Enter a complete runlist with or without spaces and be sure to end the run with 00 and press Enter." << endl;
+    cout << "Enter a complete hexadecimal runlist string with or without spaces and be sure  to end the run with 00 and press Enter." << endl << endl;
+    cout << "Enter the runlist as you see it in the hex editor, the program will account for endianess." << endl << endl;
+    cout << "Hint: Copy the hex string from the hex editor and paste it into the console."  << endl;
     cout << "-------------------------------------------------------------------------------" << endl << endl;
 
 
     //---Variable Declarations------------------------------------------------------------------------------
 
-    const int LEN = 1000;       // array size constant
-    int index = 0;              // for loop index
-    int index2 = 0;             // run array index
-    int headerIndex = 0;        // index variable to tell where the beginning of a run is within a run list
-    int clusterLengthBytes[LEN];     // right nibble of run list header, converted to integer
-    int startingExtentBytes[LEN];    // left nibble of run list header, converted to integer
-    int runListLength;
-    int jumpValue[LEN];
-    int numClusters[LEN];
-    int startCluster = 0;
+    const int LEN = 1000;           // array size constant
+    int index = 0,                  // for loop index
+        index2,                     // run array index
+        headerIndex,                // index variable to tell where the beginning of a run is within a run list
+        clusterLengthBytes[LEN],    // right nibble of run list header, converted to integer
+        startingExtentBytes[LEN],   // left nibble of run list header, converted to integer
+        runListLength,              // stores the number of characters in the run list
+        jumpValue[LEN],             // stores the signed int value of the hex run
+        numClusters[LEN],           // number of clusters in a fragment
+        startCluster = 0,           // starting cluster of a fragment
+        endCluster,                 // end cluster of a fragment
+        mask;                       // this holds value to check for two's complement
 
     char choice;
 
-    string runList,                 // stores initial run list entered by user
-           newRunList = "",         // empty string to store user run list minus any spaces
-           jumpValueHex[LEN] = "",       // array to store run jump values
-           numClustersHex[LEN] = "",
-           runHeaderL,              // string to store left nibble of run header
-           runHeaderR,              // string to store right nibble of run header
-           run[LEN],                // array to store individual runs within run list (for output)
-           lastRunByte = "",
-           runString = "";
+    string runList,                  // stores initial run list entered by user
+           newRunList,               // empty string to store user run list minus any spaces
+           jumpValueHex[LEN] = "",   // array to store run jump values
+           numClustersHex[LEN] = "", // number of fragment clusters in string hex
+           runHeaderL,               // string to store left nibble of run header
+           runHeaderR,               // string to store right nibble of run header
+           run[LEN],                 // array to store individual runs within run list (for output)
+           lastRunByte = "",         // the byte of the input run list
+           runString = "";           // temporary storage of individual run within run list
     //--------------------------------------------------------------------------------------------------------
 
     do{
 
-    cout << "Enter Run List (Hex): ";
-    getline(cin, runList);
+        // variable initializations
+        newRunList = "";
+        index2 = 0;
+        headerIndex = 0;
+        endCluster = 0;
 
-    // gets the last byte
-    runListLength = runList.length();
-    lastRunByte += runList[runListLength - 2];
-    lastRunByte += runList[runListLength - 1];
+        cout << "Enter Run List (Hex): ";
+        getline(cin, runList);
 
-    // checks for run list end byte signature 00.
-    if(lastRunByte != "00"){
+        // gets the last byte
+        runListLength = runList.length();
+        lastRunByte = runList[runListLength - 2];
+        lastRunByte += runList[runListLength - 1];
 
-        cout << "NOTE - Last byte is not 00." << endl;
-        runList += " 00";
-    }
+        // checks for run list end byte signature 00.
+        if(lastRunByte != "00"){
 
-    cout << endl;
-    cout << "Run List Processed: " << runList << endl << endl;
-
-
-    for (int i = 0; i < runList.length(); i++){
-
-        // remove spaces from input
-        if (runList[i] != ' '){
-            newRunList += runList[i];
+            cout << "NOTE - Last byte is not 00." << endl;
+            runList += " 00";
         }
-    }
 
-    // store run header
-    runHeaderL = newRunList[headerIndex];
-    runHeaderR = newRunList[headerIndex+1];
+        cout << endl;
+        cout << "Run List Processed: " << runList << endl << endl;
 
-    // exit while when end of run reached
-    while(runHeaderL != "0" && runHeaderR != "0")
-    {
 
-        // convert run header from string char to integers
-        clusterLengthBytes[index2] = convertHex(runHeaderR);
-        startingExtentBytes[index2] = convertHex(runHeaderL);
-
-        // store individual runs for output report
-        for ( index = headerIndex; index < (((clusterLengthBytes[index2] * 2) + (startingExtentBytes[index2] * 2) + 2) + headerIndex ); index++ )
+        for (int i = 0; i < runList.length(); i++)
         {
-            run[index2] += newRunList[index];
+            // remove spaces from input
+            if (runList[i] != ' ')
+            {
+                newRunList += runList[i];
+            }
         }
 
-        runString = run[index2];
-
-        // reverse the jump value to little endian notation and store each jump value into an array
-        for (int j = ( clusterLengthBytes[index2] * 2) + (startingExtentBytes[index2] * 2); j >= (clusterLengthBytes[index2]*2) + 2; j -= 2 )
-        {
-            jumpValueHex[index2] += runString[j];
-            jumpValueHex[index2] += runString[j+1];
-
-            // convert jump value string to integer and store in array
-            stringstream ss;
-            ss << hex << jumpValueHex[index2];
-            ss >> jumpValue[index2];
-            ss.str(string());
-            ss.clear();
-        }
-
-        for ( int j = (clusterLengthBytes[index2] * 2); j >= 2; j -= 2 )
-        {
-            numClustersHex[index2] += runString[j];
-            numClustersHex[index2] += runString[j+1];
-
-            stringstream ss;
-            ss << hex << numClustersHex[index2];
-            ss >> numClusters[index2];
-            ss.str(string());
-            ss.clear();
-        }
-
-        int mask = pow(2, ((startingExtentBytes[index2] * 8) - 1));
-        if ( ((jumpValue[index2] & mask) == mask) && index2 > 0)
-        {
-            mask = (pow(2, startingExtentBytes[index2] * 8) - 1);
-            jumpValue[index2] -= 1;
-            jumpValue[index2] = (jumpValue[index2] ^ mask) * -1;
-        }
-
-
-        headerIndex += ((clusterLengthBytes[index2] * 2) + (startingExtentBytes[index2] * 2) + 2);
-        index2++;
-
+        // store run header
         runHeaderL = newRunList[headerIndex];
         runHeaderR = newRunList[headerIndex+1];
-    }
 
-    for ( int j = 0; j < index2; j++ ){
+        // exit while loop when end of run reached
+        while(runHeaderL != "0" && runHeaderR != "0")
+        {
 
-        cout << "Run #" << j + 1 << ": " << endl;
+            // convert run header from string char to integers
+            clusterLengthBytes[index2] = convertHex(runHeaderR);
+            startingExtentBytes[index2] = convertHex(runHeaderL);
+
+            // store individual runs for output report
+            for ( index = headerIndex; index < (((clusterLengthBytes[index2] * 2) + (startingExtentBytes[index2] * 2) + 2) + headerIndex ); index++ )
+            {
+                run[index2] += newRunList[index];
+            }
+
+            // temp location for each run within run list
+            runString = run[index2];
+
+            // reverse the jump value to little endian notation and store each jump value into an array
+            for (int j = ( clusterLengthBytes[index2] * 2) + (startingExtentBytes[index2] * 2); j >= (clusterLengthBytes[index2]*2) + 2; j -= 2 )
+            {
+                jumpValueHex[index2] += runString[j];
+                jumpValueHex[index2] += runString[j+1];
+
+                // convert jump value string to integer and store in array
+                stringstream ss;
+                ss << hex << jumpValueHex[index2];
+                ss >> jumpValue[index2];
+                ss.str(string());
+                ss.clear();
+            }
+
+            // extract number of clusters in the fragment and store in an array in little endian
+            for ( int j = (clusterLengthBytes[index2] * 2); j >= 2; j -= 2 )
+            {
+                numClustersHex[index2] += runString[j];
+                numClustersHex[index2] += runString[j+1];
+
+                // convert number of cluster hex values to int
+                stringstream ss;
+                ss << hex << numClustersHex[index2];
+                ss >> numClusters[index2];
+                ss.str(string());
+                ss.clear();
+
+                numClustersHex[index2] = "";    // clear array for use in next run list
+            }
+
+
+            // mask will be assigned a value appropriate for the size of the run to check for negative integer notation (2's complement)
+            mask = pow(2, ((startingExtentBytes[index2] * 8) - 1));
+
+            // determines if most significant bit is 1, then converts integer to negative number by reverse 2's complement
+            if ( ((jumpValue[index2] & mask) == mask) && index2 > 0)
+            {
+                mask = (pow(2, startingExtentBytes[index2] * 8) - 1);
+                jumpValue[index2] -= 1;
+                jumpValue[index2] = (jumpValue[index2] ^ mask) * -1;
+            }
+
+            // points the header index to the next run header in the run list
+            headerIndex += ((clusterLengthBytes[index2] * 2) + (startingExtentBytes[index2] * 2) + 2);
+            index2++;
+
+            // get next run header values
+            runHeaderL = newRunList[headerIndex];
+            runHeaderR = newRunList[headerIndex+1];
+        }
+
+        cout << endl << "Individual Runs: " << endl;
         cout << "--------------------------------" << endl;
-        cout << "Run: " << run[j] << endl;
-        if (j == 0)
-        {
-            cout << "Starting Cluster in Hex: " << jumpValueHex[j] << endl;
-            cout << "Starting Cluster in Dec: " << jumpValue[j] << endl << endl;
-        }
-        else
-        {
-            cout << "Jump Value in Hex: " << jumpValueHex[j] << endl;
-            cout << "Jump Value in Dec: " << jumpValue[j] << endl << endl;
-        }
-    }
+        cout << "--------------------------------" << endl << endl;
 
-    cout << endl << "File Fragments: " << endl;
-    cout << "--------------------------------" << endl << endl;
-    startCluster = jumpValue[0];
+        // output run values
+        for ( int j = 0; j < index2; j++ ){
 
-    for ( int j = 0; j < index2; j++)
-    {
-        cout << "Fragment " << j + 1 << ": " << endl << endl;
-        if ( j == 0 )
-        {
-            int endCluster = jumpValue[j];
-            endCluster += numClusters[j];
-            cout << "Clusters " << jumpValue[j] << " - " << endCluster << endl << endl;
+            cout << "Run #" << j + 1 << ": " << endl;
+            cout << "--------------------------------" << endl;
+            cout << "Run: " << run[j] << endl << endl;
+            run[j] = "";
+            if (j == 0)
+            {
+                cout << "Starting Cluster in Hex: " << jumpValueHex[j] << endl << endl;
+                cout << "Starting Cluster in Dec: " << jumpValue[j] << endl << endl;
+            }
+            else
+            {
+                cout << "Jump Value in Hex: " << jumpValueHex[j] << endl << endl;
+                cout << "Jump Value in Dec: " << jumpValue[j] << endl << endl;
+            }
+
+            jumpValueHex[j] = "";       // clear array for use in next run list
         }
 
-        else
-        {
+        // output fragment information
+        cout << endl << "File Fragments: " << endl;
+        cout << "--------------------------------" << endl;
+        cout << "--------------------------------" << endl << endl;
 
-            startCluster += jumpValue[j];
-            int endCluster = startCluster + numClusters[j];
-            cout << "Clusters " << startCluster << " - " << endCluster << endl << endl;
+        startCluster = jumpValue[0];
+
+        for ( int j = 0; j < index2; j++)
+        {
+            cout << "Fragment " << j + 1 << ": " << endl << endl;
+            if ( j == 0 )
+            {
+                endCluster = jumpValue[j];
+                endCluster += numClusters[j];
+                cout << "Clusters " << jumpValue[j] << " - " << endCluster << endl << endl;
+                cout << "Total clusters in fragment: " << numClusters[j] << endl << endl;
+            }
+
+            else
+            {
+
+                startCluster += jumpValue[j];
+                endCluster = startCluster + numClusters[j];
+                cout << "Clusters " << startCluster << " - " << endCluster << endl << endl;
+                cout << "Total clusters in fragment: " << numClusters[j] << endl << endl;
+            }
+
+            // clear arrays for use in next run list
+            jumpValue[j] = 0;
+            numClusters[j] = 0;
+
         }
 
-    }
-
-    cout << "Process another runlist (y/n)?";
-    cin.get(choice);
-    cout << endl;
-    cin.ignore();
+        cout << endl << "Process another runlist (y/n)?";
+        cin.get(choice);
+        cout << endl;
+        cin.ignore();
 
     }while (choice == 'y' || choice == 'Y');
 
@@ -209,6 +247,7 @@ int main(){
     return 0;
 }
 
+// this function converts the run header values to integers
 int convertHex(string runHeader){
 
 
